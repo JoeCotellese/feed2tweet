5ea58387fa007e150a96c73722506e3a055e35c8
Joe Cotellese <joe@cotellese.net>
2022-02-05 08:39:02 -0500

    update to store & read last tweet date

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 require('dotenv').config();
 const JSSoup = require('jssoup').default;
 const axios = require('axios');
+const fs = require('fs/promises');
+const homedir = require('os').homedir();
 const { TwitterApi } = require('twitter-api-v2');
-const { default: TwitterApiSubClient } = require('twitter-api-v2/dist/client.subclient');
 
 axios.get(process.env.FEED_URL)
     .then(response => {
-        recentEntries = fetchRecentFeedEntries(response.data.items)
-        publishTweets(recentEntries)
+        processFeed(response.data.items)
     })
     .catch(error => {
         console.error(error)
     })
 
-function fetchRecentFeedEntries(feed) {
+function processFeed(feed) {
+    lastRunDate = new Date()
+    fs.readFile(`${homedir}/.feed2tweet/rundate`)
+    .then(data => {
+        lastRunDate = new Date(data)
+        console.log(`Last run date: ${lastRunDate}`)
+    })
+    .catch(error => {
+        lastRunDate = new Date(new Date().getTime() - (2 * 60 * 60 * 1000))
+        console.log(`No last run date found. Setting to ${lastRunDate}`)
+    })
+    .finally(() => {
+        const recentEntries = fetchRecentFeedEntries(feed, lastRunDate)
+        publishTweets(recentEntries)
+        fs.writeFile(`${homedir}/.feed2tweet/rundate`, new Date(Date.now()).toISOString())
+    })
+}
+
+
+function fetchRecentFeedEntries(feed, lastRunDate) {
+    console.log(`Fetching recent entries since ${lastRunDate}`)
     return feed.filter(entry => {
-        const oneHourAgo = new Date(new Date().getTime() - (2 * 60 * 60 * 1000))
         const postDate = new Date(entry.date_published)
-        if (postDate > oneHourAgo) {
+        if (postDate > lastRunDate) {
             return entry
         }
     })
@@ -34,10 +53,10 @@ async function publishTweets(tweets) {
     twitterClient.currentUserV2()
     .then(user => console.log(user))
     tweets.forEach(tweet => {
-        console.log(tweet)
         var soup = new JSSoup(tweet.content_html)
-        console.log(soup)
         var tweetText = soup.find('p').text
-        twitterClient.v1.tweet(tweetText).then(tweet => console.log(tweet))
+        console.log(`publishing tweet: ${tweetText}`)
+        // twitterClient.v1.tweet(tweetText).then(tweet => console.log(tweet))
     })
 }
+
